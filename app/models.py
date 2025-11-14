172202b0333143de7564c1d9ae8f0c5f7d4bfa37
Beatrice Gachigi <beatricegachigi@gmail.com>
2025-11-14 11:01:07 +0300

    chore(migrations):exceed values on charfield

--- app/models.py
+++ app/models.py
@@ -5,10 +5,10 @@ from django.utils import timezone
 
 class Template(models.Model):
     template_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    name = models.CharField(max_length=255)
-    version = models.CharField(max_length=10, default="latest")
-    language = models.CharField(max_length=2, default="en")
-    type = models.CharField(max_length=10)
+    name = models.CharField(max_length=2550000)
+    version = models.CharField(max_length=10000000, default="latest")
+    language = models.CharField(max_length=2000000, default="en")
+    type = models.CharField(max_length=10000000)
     subject = models.TextField(blank=True, null=True)
 
     body = models.JSONField(default=dict)
@@ -24,11 +24,3 @@ class Template(models.Model):
     def __str__(self):
         return f"{self.name} ({self.language}) v{self.version}"
 
-    def save(self, *args, **kwargs):
-        # Automatically truncate all CharFields to their max_length
-        for field in self._meta.fields:
-            if isinstance(field, models.CharField):
-                value = getattr(self, field.name)
-                if value and field.max_length:
-                    setattr(self, field.name, value[:field.max_length])
-        super().save(*args, **kwargs)
